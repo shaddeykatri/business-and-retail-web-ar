d8dd9fa5ec0a058375fb77527ed73db7fa608d95
Sachit Kumar Katri <31386786+shaddeykatri@users.noreply.github.com>
2021-11-17 00:20:49 +0530

    added few routes

--- controller/productController.js
+++ controller/productController.js
@@ -5,7 +5,6 @@ const asyncHandler = require("../middleware/async");
 exports.addProduct = asyncHandler(async (req, res, next) => {
   var Product;
   if (req.params["industry"].localeCompare("Fashion") == 0) {
-    console.log("inside fashion")
     Product = new Fashion({
       name: req.body.name,
       description: req.body.description,
@@ -15,10 +14,9 @@ exports.addProduct = asyncHandler(async (req, res, next) => {
       category: req.body.category,
       product_image_url: req.body.product_image_url,
       price: req.body.price,
-      industry: req.params["industry"]
+      industry: req.params["industry"],
     });
   } else if (req.params["industry"].localeCompare("Furniture") == 0) {
-    console.log("inside furniture")
     Product = new Furniture({
       name: req.body.name,
       description: req.body.description,
@@ -28,10 +26,9 @@ exports.addProduct = asyncHandler(async (req, res, next) => {
       category: req.body.category,
       product_image_url: req.body.product_image_url,
       price: req.body.price,
-      industry: req.params["industry"]
+      industry: req.params["industry"],
     });
   } else if (req.params["industry"].localeCompare("Machinery") == 0) {
-      console.log("inside machinery")
     Product = new Machinery({
       name: req.body.name,
       description: req.body.description,
@@ -41,10 +38,10 @@ exports.addProduct = asyncHandler(async (req, res, next) => {
       category: req.body.category,
       product_image_url: req.body.product_image_url,
       price: req.body.price,
-      industry: req.params["industry"]
+      industry: req.params["industry"],
     });
   }
- await Product.save()
+  await Product.save()
     .then((data) => {
       res.status(201).send({ result: data });
     })
@@ -56,186 +53,114 @@ exports.addProduct = asyncHandler(async (req, res, next) => {
     });
 });
 
-//To get all the models
-exports.getAllProducts = (req, res) => {
-  modelViewer
-    .find()
-    .sort("-createdAt")
-    .exec((err, models) => {
-      // error checking
-      if (err || !models) {
-        //   logger.error("Bad request. Models cannot be found")
-        return res.status(400).json({
-          result: "Something went wrong in finding all models",
-        });
-      }
-      // logger.info("All models found")
-      // return all the model in json format
-      res.status(200).json({ result: models });
-    });
-};
+exports.updateProduct = asyncHandler(async (req, res, next) => {
+  var Product;
 
-//To get all the model by modelid
-exports.getmodel = (req, res) => {
-  modelViewer
-    .findById(req.params.id)
-    .then((model) => {
-      if (!model) {
-        //   logger.warn("The modelId " + req.params.id + " does not exist")
-        return res.status(404).send({
-          result: "model not found with id " + req.params.id,
-        });
-      }
-
-      res.status(200).send({ result: model });
-      // logger.info("model found with id " + req.params.id)
-    })
-    .catch((err) => {
-      if (err.kind === "ObjectId") {
-        //   logger.error("Incorrect modelId " + req.params.id)
-        return res.status(404).send({
-          result: "model not found with id " + req.params.id,
-        });
-      }
-      // logger.error("Error getting model with id " + req.params.id)
-      return res.status(500).send({
-        result: "Error getting model with id " + req.params.id,
-      });
-    });
-};
-
-//To update a model
-exports.updatemodel = (req, res) => {
-  // Validate Request
-
-  if (!req.body) {
-    //   logger.warn("Incomplete Body")
-    return res.status(400).send({
-      result: "Please fill all required field",
-    });
+  if (req.params["industry"].localeCompare("Fashion") == 0) {
+    Product = await Fashion.findById(req.params["id"]);
+  } else if (req.params["industry"].localeCompare("Furniture") == 0) {
+    Product = await Furniture.findById(req.params["id"]);
+  } else if (req.params["industry"].localeCompare("Machinery") == 0) {
+    Product = await Machinery.findById(req.params["id"]);
   }
-  // Find model and update it with the request body
-  model
-    .findByIdAndUpdate(
-      req.params.id,
-      {
-        IosSrc: req.IosSrc,
-        Src: req.body.Src,
-        Description: req.body.Description,
-        Model_ID: req.body.Model_ID,
-        Color: req.body.Color,
-        SubCategory: req.body.SubCategory,
-        Product_Image: req.body.Product_Image,
-        PriceText: req.body.PriceText,
-        Price: req.body.Price,
-        Name: req.body.Name,
-        Short_Description: req.body.Short_Description,
-        variants: req.body.variants,
-      },
-      { new: true }
-    )
-    .then((model) => {
-      if (!model) {
-        //   logger.warn("The modelId " + req.params.id + " does not exist")
-        return res.status(404).send({
-          result: "model not found with id " + req.params.id,
-        });
-      }
-      // logger.info("The model with modelId " + req.params.id + " is updated")
-      res.status(201).send({ result: model });
-    })
-    .catch((err) => {
-      if (err.kind === "ObjectId") {
-        //   logger.error("Incorrect modelId " + req.params.id)
-        return res.status(404).send({
-          result: "model not found with id " + req.params.id,
-        });
-      }
-      // logger.error("Error updating model with id " + req.params.id)
-      return res.status(500).send({
-        result: "Error updating model with id " + req.params.id,
-      });
-    });
-};
+  console.log(Product)
 
-//To delete a model
-exports.deletemodel = (req, res) => {
-  modelViewer
-    .findByIdAndRemove(req.params.id)
-    .then((model) => {
-      if (!model) {
-        //   logger.warn("The modelId " + req.params.id + " does not exist")
-        return res.status(404).send({
-          result: "model not found with id " + req.params.id,
-        });
-      }
-      // logger.info("The model with modelId " + req.params.id + " is deleted")
-      res.status(200).send({ result: "model deleted successfully!" });
-    })
-    .catch((err) => {
-      if (err.kind === "ObjectId" || err.name === "NotFound") {
-        //   logger.error("Incorrect modelId " + req.params.id)
-        return res.status(404).send({
-          result: "model not found with id " + req.params.id,
-        });
-      }
-      // logger.error("Could not delete model with id " + req.params.id)
-      return res.status(500).send({
-        result: "Could not delete model with id " + req.params.id,
-      });
-    });
-};
+    Product.name = req.body.name,
+    Product.description = req.body.description,
+    Product.short_description = req.body.short_description,
+    Product.ios_src = req.body.ios_src,
+    Product.src = req.body.src,
+    Product.category = req.body.category,
+    Product.product_image_url = req.body.product_image_url,
+    Product.price = req.body.price,
+    Product.industry = req.params["industry"]
 
-// to get modelby category
-exports.getmodelbycategory = (req, res) => {
-  modelViewer
-    .find({ SubCategory: req.params.category })
-    .sort("-createdAt")
-    .then((model) => {
-      if (!model) {
-        //logger.warn("The workOrderId " + req.params.id + " does not exist")
-        return res.status(404).send({
-          result: "model not found with category " + req.params.category,
+    await Product.save()
+      .then((data) => {
+        res.status(201).send({ result: data });
+      })
+      .catch((err) => {
+        //   logger.error("Something went wrong while creating new model.")
+        res.status(500).send({
+          result:
+            err.message || "Something went wrong while updating new model.",
         });
-      }
-      //logger.info("models with workOrdercategory " + req.params.category + " found successfully")
-      res.status(200).send({ result: model });
-    })
-    .catch((err) => {
-      if (err.kind === "String") {
-        //logger.error("Incorrect workOrdercategory " + req.params.category)
-        return res.status(404).send({
-          result: "model not found with category " + req.params.category,
-        });
-      }
-      //logger.error("Error getting model with category "+ req.params.category)
-      return res.status(500).send({
-        result: "Error getting model with category " + req.params.category,
       });
-    });
-};
+});
 
-// to get modelby modelid
-exports.getmodelbymodelid = (req, res) => {
-  modelViewer
-    .find({ Model_ID: req.params.id })
-    .sort("-createdAt")
-    .then((model) => {
-      if (!model) {
-        //logger.warn("The workOrderId " + req.params.id + " does not exist")
-        return res.status(404).send("model not found with id " + req.params.id);
+exports.getAllProducts = asyncHandler(async (req, res, next) => {
+  var Products;
+
+  if (req.params["industry"].localeCompare("Fashion") == 0) {
+    Products = await Fashion.find();
+  } else if (req.params["industry"].localeCompare("Furniture") == 0) {
+    Products = await Furniture.find();
+  } else if (req.params["industry"].localeCompare("Machinery") == 0) {
+    Products = await Machinery.find();
+  }
+  res.status(200).json({
+    Products
+  });
+});
+
+exports.deleteProductById = asyncHandler(async (req, res, next) => {
+    var Products;
+    // var industry = req.params["industry"]
+    if (req.params["industry"].localeCompare("Fashion") == 0) {
+      Products = await Fashion.deleteOne({_id:req.params["id"]});
+    } else if (req.params["industry"].localeCompare("Furniture") == 0) {
+      Products = await Furniture.deleteOne({_id:req.params["id"]});
+    } else if (req.params["industry"].localeCompare("Machinery") == 0) {
+      Products = await Machinery.deleteOne({_id:req.params["id"]});
+    }
+
+    // Products = await industry.deleteOne({_id:req.params["id"]});
+
+    if(Products != null){
+        res.status(200).json({
+          Message: `Product successfully deleted!`,
+        });
       }
-      //logger.info("models with workOrderid " + req.params.id + " found successfully")
-      res.status(200).send(model);
-    })
-    .catch((err) => {
-      if (err.kind === "String") {
-        //logger.error("Incorrect workOrderid " + req.params.id)
-        return res.status(404).send("model not found with id " + req.params.id);
+      else{
+          res.status(400).json({
+              Message: "Could not find a product with this id."
+          })
       }
-      //logger.error("Error getting model with id "+ req.params.id)
-      return res
-        .status(500)
-        .send("Error getting model with id " + req.params.id);
-    });
-};
+});
+
+
+exports.getProductById = asyncHandler(async (req, res, next) => {
+    var Product;
+
+    if (req.params["industry"].localeCompare("Fashion") == 0) {
+      Product = await Fashion.findById(req.params["id"]);
+    } else if (req.params["industry"].localeCompare("Furniture") == 0) {
+      Product = await Furniture.findById(req.params["id"]);
+    } else if (req.params["industry"].localeCompare("Machinery") == 0) {
+      Product = await Machinery.findById(req.params["id"]);
+    }
+
+    res.status(200).json({
+        Product
+      });
+
+});
+
+exports.getProductsByCategory = asyncHandler(async (req, res, next) => {
+    var Product;
+
+    if (req.params["industry"].localeCompare("Fashion") == 0) {
+      Product = await Fashion.find({category: req.params["category"]});
+    } else if (req.params["industry"].localeCompare("Furniture") == 0) {
+      Product = await Furniture.find({category: req.params["category"]});
+    } else if (req.params["industry"].localeCompare("Machinery") == 0) {
+      Product = await Machinery.find({category: req.params["category"]});
+    }
+    res.status(200).json({
+        Product
+      });
+});
+
+exports.getAllCategories = asyncHandler(async (req, res, next) => {
+    
+});
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,20 +1,22 @@
-const express = require('express');
-const { addProduct } = require('../controller/productController');
-
+const express = require("express");
+const {
+  addProduct,
+  updateProduct,
+  getAllProducts,
+  deleteProductById,
+  getProductById,
+  getProductsByCategory,
+} = require("../controller/productController");
 
 const router = express.Router();
 
+router.get("/:industry/getAll", getAllProducts);
+router.get('/:industry/:id',  getProductById);
+router.get('/:industry/:category/getAll',  getProductsByCategory);
+router.get('/:industry/getAllCategories',  getAllCategories);
 
-
-// router.get('/getAll',  getAllProducts);
-
-// router.get('/:industry/getAll',  getAssetById);
-// router.get('/:industry/:id',  getAssetById);
-// router.get('/:industry/:category/getAll',  getAssetById);
-// router.get('/:industry/getAllCategories',  getAssetById);
-
-router.post('/:industry/addProduct',  addProduct);
-// router.put('/:industry/updateProduct/:id', putAssetById);
-// router.delete('/:industry/deleteProduct/:id', deleteExternalAsset);
+router.post("/:industry/addProduct", addProduct);
+router.put("/:industry/updateProduct/:id", updateProduct);
+router.delete('/:industry/deleteProduct/:id', deleteProductById);
 
 module.exports = router;
